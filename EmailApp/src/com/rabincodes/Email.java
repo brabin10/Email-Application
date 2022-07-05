ef16a6bb53a755991950dc19c213a437256e5b2d
Rabin Basnet <rowbinr@yahoo.com>
2022-07-05 16:23:26 -0600

    generating random password

--- EmailApp/src/com/rabincodes/Email.java
+++ EmailApp/src/com/rabincodes/Email.java
@@ -19,10 +19,12 @@ public class Email {
 		
 		// Call a method asking for the department - return the department
 		this.department = setDepartment();
+		
+		// Call a method that returns a random password
 	}
 	// Ask for the department
 	private String setDepartment() {
-		System.out.println("Enter the department\n1 for Sales\n2 for Development\n3 for Accounting\n0 for None");
+		System.out.println("Department Codes\n1 for Sales\n2 for Development\n3 for Accounting\n0 for None\nEnter department code:");
 		Scanner in = new Scanner(System.in);
 		int depChoice = in.nextInt();
 		if (depChoice == 1) {return "Sales";}
@@ -30,13 +32,22 @@ public class Email {
 		else if (depChoice == 3) {return "acct";}
 		else { return ""; }
 		}
-	}
+	
 	// Generate a random password
+	private String randomPassword(int length) {
+		String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789$!@#%";
+		char[] password = new char[length];
+		for(int i = 0; i< length; i++) {
+			int rand = (int)Math.random()* passwordSet.length();
+			password[i] = passwordSet.charAt(rand);
+		}
+		return new String(password);
+	}
 	
 	// Set the mailbox capacity
 	
 	// Set the alternate email
 	
 	// Change the password
-}
 
+}
