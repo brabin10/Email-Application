a1f749835b59b94c127b6a2cabe398a04370f0ad
Rabin Basnet <rowbinr@yahoo.com>
2022-07-05 16:37:54 -0600

    random password generated

--- EmailApp/src/com/rabincodes/Email.java
+++ EmailApp/src/com/rabincodes/Email.java
@@ -10,6 +10,8 @@ public class Email {
 	private String department;
 	private String alternateEmail;
 	private int mailboxCapacity;
+	private int defaultPasswordLength = 10;
+
 	
 	// Constructor to receive the first name and last name
 	public Email(String firstName, String lastName) {
@@ -19,8 +21,11 @@ public class Email {
 		
 		// Call a method asking for the department - return the department
 		this.department = setDepartment();
+		System.out.println("Department: " + this.department);
 		
 		// Call a method that returns a random password
+		this.password = randomPassword(defaultPasswordLength);
+		System.out.println("Your password is: " + this.password);
 	}
 	// Ask for the department
 	private String setDepartment() {
@@ -38,8 +43,9 @@ public class Email {
 		String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789$!@#%";
 		char[] password = new char[length];
 		for(int i = 0; i< length; i++) {
-			int rand = (int)Math.random()* passwordSet.length();
+			int rand = (int)(Math.random()* passwordSet.length());
 			password[i] = passwordSet.charAt(rand);
+			
 		}
 		return new String(password);
 	}
