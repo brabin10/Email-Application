f15348b9e5037770481896883297826a9d76406e
Rabin Basnet <rowbinr@yahoo.com>
2022-07-05 01:00:10 -0600

    setting the department

--- EmailApp/src/com/rabincodes/Email.java
+++ EmailApp/src/com/rabincodes/Email.java
@@ -1,5 +1,7 @@
 package com.rabincodes;
 
+import java.util.Scanner;
+
 public class Email {
 
 	private String firstName;
@@ -14,9 +16,21 @@ public class Email {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		System.out.println("Email created: " + this.firstName + " " + this.lastName);
+		
+		// Call a method asking for the department - return the department
+		this.department = setDepartment();
 	}
 	// Ask for the department
-	
+	private String setDepartment() {
+		System.out.println("Enter the department\n1 for Sales\n2 for Development\n3 for Accounting\n0 for None");
+		Scanner in = new Scanner(System.in);
+		int depChoice = in.nextInt();
+		if (depChoice == 1) {return "Sales";}
+		else if (depChoice == 2) {return "dev";}
+		else if (depChoice == 3) {return "acct";}
+		else { return ""; }
+		}
+	}
 	// Generate a random password
 	
 	// Set the mailbox capacity
